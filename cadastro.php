f03e4fb49dfbb1001dccd9b15da4140616d74590
matheusdantas01 <78809049+matheusdantas01@users.noreply.github.com>
2022-02-04 21:39:37 -0300

    cadasro.php

---
+++ cadastro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cadastro de usuários</title>
+</head>
+<body>
+    <head>
+     <h1>Cadastro de usuários</h1>
+        <p>Criação de sistema com armazenamento em banco de dados utilizando PHP</p>
+    </head>
+
+
+
+<a href="index.php">Voltar</a>
+
+<form action="/save.php"  method="post">
+  <label for="nome">Escreva seu nome:</label><br>
+  <input type="text" id="nome" name="nome"><br>
+  <label for="email">Escreva seu E-mail:</label><br>
+  <input type="text" id="email" name="email"><br>
+  <label for="senha">Digite sua senha:</label><br>
+  <input type="password" id="senha" name="senha"><br>
+  <input type="submit" value="Finalizar cadastro">
+</form>
+
+<footer>
+    <p>SISTEMA BDWeb</p>
+    <p> Author: Matheus Bonfim Dantas<br>
+        E-mail: <a href = "matheus.dantas.075@gmail.com">matheus.dantas.075@gmail.com</a></p>
+</footer>
+
+</html>
