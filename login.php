f368091e67959b7d4acd2766a4e6ae840c0b02c6
matheusdantas01 <78809049+matheusdantas01@users.noreply.github.com>
2022-02-04 21:41:54 -0300

    login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login de usuários</title>
+</head>
+<body>
+    <head>
+     <h1>Login</h1>
+        <p>Criação de sistema para disciplina de Banco de Dados para WEB</p>
+    </head>
+
+<form action="validar_usuario.php"  method="Post">
+  <label for="email">Escreva seu E-mail:</label><br>
+  <input type="text" id="email" name="email"><br>
+  <label for="senha">Digite sua senha:</label><br>
+  <input type="password" id="senha" name="senha"><br>
+  <input type="submit" value="Realizar login">
+</form>
+
+<footer>
+    <p>SISTEMA BDWeb</p>
+    <p> Author: Matheus Bonfim Dantas<br>
+        E-mail: <a href = "matheus.dantas.075@gmail.com">matheus.dantas.075@gmail.com</a></p>
+</footer>
+
+</html>
+
+    <a href = "home.php">voltar</a>
+</body>
+</html>
